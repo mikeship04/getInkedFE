3b538e8873861de461e6ea06304a168248925f98
Mike Shippy <mikeship04@gmail.com>
2022-08-29 13:52:54 -0700

    first fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 
 function App() {
+  fetch('http://localhost:4000/hello')
+  .then((res) => res.json())
+  .then(console.log)
+  
   return (
     <div className="App">
       Welcome to getInked!
