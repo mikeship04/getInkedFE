2c803f8e19f0a67093b7fc76585354179f5ae477
Mike Shippy <mikeship04@gmail.com>
2022-08-31 08:38:43 -0700

    added navbar

---
+++ src/Components/AboutUs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div>About_us</div>
+  )
+}
+
+export default AboutUs
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,6 +4,7 @@ import {
     FormLabel,
     Input,
     Button,
+    Container,
 } from '@chakra-ui/react'
 
 function Login() {
@@ -12,12 +13,16 @@ function Login() {
         password: ""
     })
 
+    const userObj = {
+        user: {...formObj}
+    }
+
     function handleSubmit(e){
         e.preventDefault()
-        fetch('http://localhost:3000/login',{
+        fetch('http://localhost:9292/login',{
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(formObj)
+            body: JSON.stringify(userObj)
         })
         .then(res => {
             res.json()
@@ -31,15 +36,17 @@ function Login() {
     }
 
 return (
+    <Container maxW='md'>
     <form onSubmit={handleSubmit}>
     <FormControl isRequired>
     <FormLabel>Username</FormLabel>
     <Input id='username' type='username' value={formObj.username} onChange={handleChange} />
     <FormLabel>Password</FormLabel>
     <Input id='password' type='password' value={formObj.password} onChange={handleChange}/>
-    <Button mt={4} colorScheme='teal' type='submit'> Submit </Button>
+    <Button mt={4} variant="primary" type='submit'> Submit </Button>
     </FormControl>
     </form>
+    </Container>
 )
 }
 
